0ce24ef0f9e6dd31be3cea8434c1a0db87b91852
avcexyegonet <grigoriy.solonko@gmail.com>
2021-03-31 14:29:37 +0300

    master | 6

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,3 @@
-const keyCodesRus = {
-    0: " ",
-    65: "ф",
-    66: "и",
-    67: "с",
-    68: "в",
-    69: "у",
-    70: "а",
-    71: "п",
-    72: "р",
-    73: "ш",
-    74: "о",
-    75: "л",
-    76: "д",
-    77: "ь",
-    78: "т",
-    79: "щ",
-    80: "з",
-    81: "й",
-    82: "к",
-    83: "ы",
-    84: "е",
-    85: "г",
-    86: "м",
-    87: "ц",
-    88: "ч",
-    89: "н",
-    90: "я",
-    186: "ж",
-    188: "б",
-    190: "ю",
-    219: "х",
-    221: "ъ",
-    222: "э",
-    192: "ё"
-};
-
-const keyCodesEn = {
-    65: "a",
-    66: "b",
-    67: "c",
-    68: "d",
-    69: "e",
-    70: "f",
-    71: "g",
-    72: "h",
-    73: "i",
-    74: "j",
-    75: "k",
-    76: "l",
-    77: "m",
-    78: "n",
-    79: "o",
-    80: "p",
-    81: "q",
-    82: "r",
-    83: "s",
-    84: "t",
-    85: "u",
-    86: "v",
-    87: "w",
-    88: "x",
-    89: "y",
-    90: "z",
-    186: ";",
-    188: ",",
-    190: ".",
-    219: "[",
-    221: "]",
-    222: "'",
-    192: "`"
-}
-
 const getSearchParams = () => {
     var url = window.location.search;
     let params = url.substring(url.lastIndexOf('?') + 1);
@@ -81,18 +8,16 @@ const getSearchParams = () => {
 class PrintText {
     constructor() {
         this.textWrap = document.getElementById('text');
-        this.titleTr = '';
-        this.messageTr = '';
+        this.title = '';
+        this.message = '';
         this.langIsEng = false;
     }
 
     init() {
+        console.log('init')
         this.getText();
     }
 
-    /**
-     * "t=заголовок&m=текст" "link= получение ссылки"
-     */
     getText() {
         let searchParams = getSearchParams();
 
@@ -110,48 +35,35 @@ class PrintText {
 
         // если есть заголовок
         if (searchParams.has("t")) {
-            this.titleTr = this.translateText(searchParams.get("t"))
+            this.title = searchParams.get("t")
         }
 
         // если есть текст
         if (searchParams.has("m")) {
-            this.messageTr = this.translateText(searchParams.get("m"))
+            this.message = searchParams.get("m")
         }
 
         this.showText();
     }
 
-    //  ?t=71-72-66-68-84-78-0-71-72-66-68-84-78&m=71-72-66-68-84-78-0-71-72-66-68-84-78
-    translateText(text) {
-        let textArr = text.split('-');
-        let result = textArr.map(num => {
-
-            if (this.langIsEng) {
-                return keyCodesEn[+num];
-            }
-
-            return keyCodesRus[+num];
-        })
-
-        return result.join("");
-    }
-
     showText() {
 
-        if (this.titleTr) {
+        console.log(this.title)
+        console.log(this.message)
+
+        if (this.title) {
             let title = document.createElement('h1');
-            title.innerHTML = this.titleTr;
+            title.innerHTML = this.title;
             this.textWrap.appendChild(title);
         }
 
-        if (this.messageTr) {
+        if (this.message) {
             let message = document.createElement('p');
-            message.innerHTML = this.messageTr;
+            message.innerHTML = this.message;
             this.textWrap.appendChild(message);
         }
     }
 }
-
 
 class CreateLink {
     constructor() {
@@ -161,6 +73,7 @@ class CreateLink {
         this.inputTitle;
         this.inpitMessage;
         this.buttonSubmit;
+        this.linkCounter = 1;
     }
 
     init() {
@@ -185,31 +98,30 @@ class CreateLink {
         this.form.appendChild(this.buttonSubmit);
 
         this.buttonSubmit.addEventListener('click', () => {
-            this.title = this.inputTitle.value.toLowerCase();
-            this.message = this.inputMessage.value.toLowerCase();
+            this.title = this.inputTitle.value
+            this.message = this.inputMessage.value
             this.createLink();
         })
     }
 
     createLink() {
-        let titleTr;
-        let messageTr;
-
         // если есть заголовок
         if (this.title) {
-            titleTr = this.translateLink(this.title)
+            this.title = encodeURI(this.title)
         }
 
         // если есть текст
         if (this.message) {
-            messageTr = this.translateLink(this.message)
+            this.message = encodeURI(this.message)
         }
-
+    
         let oldLink = document.getElementById('link');
-        let link = `https://avcexyegonet.github.io/dist/?t=${titleTr}&m=${messageTr}`;
+        let link = `https://avcexyegonet.github.io/dist/?t=${this.title}&m=${this.message}`;
 
         if (oldLink) {
             oldLink.setAttribute('href', link);
+            this.linkCounter++;
+            oldLink.innerHTML = `link ${this.linkCounter}`;
         } else {
             let linkEl = document.createElement('a');
             linkEl.innerHTML = 'link';
@@ -219,29 +131,7 @@ class CreateLink {
             this.form.appendChild(linkEl);
         }
     }
-
-    translateLink(text) {
-        let textArr = text.split('');
-
-        let result = textArr.map(symb => {
-            let source = keyCodesRus;
-
-            if (this.langIsEng) {
-                source = keyCodesEn;
-            }
-
-            for (let num in source) {
-                if (source[num] === symb) {
-                    return +num;
-                }
-            }
-        })
-
-        return result.join("-");
-    }
 }
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     let text = new PrintText();
